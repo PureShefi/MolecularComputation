b2c75e66b3b196e8a670c6c719816fa52e54a786
Zohar Shefi <wwzohar@gmail.com>
2022-01-23 23:45:38 +0200

    Added deep copy to relevant places

--- 3sat.py
+++ 3sat.py
@@ -3,7 +3,7 @@ import itertools
 import random
 import copy
 
-SIZE = 6
+SIZE = 8
 
 variable_to_dna = {}
 edges_to_node = {}
@@ -19,9 +19,13 @@ def parse_sat_formula(formula):
 
     return sats
 
-def get_dna_value(var):
+def get_dna_value(var, create=False):
     """ Create a unique dna sequence for each variable """
     global variable_to_dna
+    if not create:
+        return copy.deepcopy(variable_to_dna[var])
+
+    # If we don't care about creating run in try clause which genereates dna if it doesn't exist
     try:
         return copy.deepcopy(variable_to_dna[var])
 
@@ -36,7 +40,6 @@ def get_all_dna_pairs(sats):
     return [x for y in sats for x in [y, "~"+y]]
 
 def get_nodes_from_edges(variables):
-    global variable_to_dna
     nodes = []
     # Run on each variable
     for i, clean_var in enumerate(variables):
@@ -44,30 +47,30 @@ def get_nodes_from_edges(variables):
         for prefix in ["", "~"]:
             # Get the wanted variable
             var = prefix + clean_var
-            half_len = len(variable_to_dna[var].sequence[0])//2
+            half_len = len(get_dna_value(var).sequence[0])//2
 
             # If we are on the first var, we need a short sequence to fill the start.
             # But it should also create a connection to the next dna
             if i == 0:
-                base = variable_to_dna[var]
+                base = get_dna_value(var)
                 sequence = base.sequence[0][:half_len]
                 nodes.append(DNA("", DNA.get_pair_string(sequence)))
 
             # The last dna only needs half a sequence to fill the second half of the last dna
             if i == len(variables)-1:
-                base = variable_to_dna[var]
+                base = get_dna_value(var)
                 sequence = base.sequence[0][half_len:]
                 nodes.append(DNA("", DNA.get_pair_string(sequence)))
                 continue
 
             # All dna except the last should have a connection to the next variable (Positive or Negative)
-            tail, head = variable_to_dna[var], variable_to_dna[variables[i+1]]
+            tail, head = get_dna_value(var), get_dna_value(variables[i+1])
             sequence = tail.sequence[0][half_len:] + head.sequence[0][:half_len]
             d = DNA("", DNA.get_pair_string(sequence))
             nodes.append(d)
 
             # Add connection from current variable to the next one as negative
-            tail, head = variable_to_dna[var], variable_to_dna["~"+variables[i+1]]
+            tail, head = get_dna_value(var), get_dna_value("~"+variables[i+1])
             sequence = tail.sequence[0][half_len:] + head.sequence[0][:half_len]
             nodes.append(DNA("", DNA.get_pair_string(sequence)))
 
@@ -78,17 +81,27 @@ def filter_by_sat(lab, sat):
         Get a lab of samples, and check if it verifies the sat
         On each group of 3 of the sats, extract only the one that satisfies at least one of the variables
     """
-    global variable_to_dna
     # Run on all Ci
     for x,y,z in sat:
         # Check if the samples satisfy the given 3 variables
-        leftovers1 = Lab(lab.extract(variable_to_dna[x].sequence[0]))
-        leftovers2 = Lab(leftovers1.extract(variable_to_dna[y].sequence[0]))
-        leftovers2.extract(variable_to_dna[z].sequence[0])
+        leftovers1 = Lab(lab.extract(get_dna_value(x).sequence[0]))
+        leftovers2 = Lab(leftovers1.extract(get_dna_value(y).sequence[0]))
+        leftovers2.extract(get_dna_value(z).sequence[0])
 
         lab.samples += leftovers1.samples + leftovers2.samples
 
     return lab
+
+def sanitize_random_edges(edges):
+    """ Make sure there are no two similar edges """
+    for x in edges:
+        for y in edges:
+            v_x = x.sequence[0]
+            v_y = y.sequence[0]
+            half_len = len(v_x)//2
+            if x != y:
+                assert v_x[:half_len] != v_y[:half_len], "x:y - {}:{}".format(x, y)
+                assert v_x[half_len:] != v_y[half_len:], "x:y - {}:{}".format(x, y)
 
 def three_sat(formula):
     """ Calculate the given 3Sat formula using molecular programming """
@@ -100,9 +113,12 @@ def three_sat(formula):
 
     # Generate the needed dna's from the formula
     print("[*] Generating edges and nodes based on the formula")
-    edges = [get_dna_value(x) for x in pairs]
+    edges = [get_dna_value(x, True) for x in pairs]
     nodes = get_nodes_from_edges(variables)
-    samples = edges + nodes
+    samples = copy.deepcopy(edges) + nodes
+
+    # Make sure are generated dna's are unique enough
+    sanitize_random_edges(edges)
 
     # Create our lab from the given dna's for the formula
     print("[*] Creating the lab from the samples. Size: {}".format(len(samples)))
@@ -110,22 +126,29 @@ def three_sat(formula):
 
     # Amplify all samples using pcr. Split after running because we need to connect the edges and nodes
     print("[*] Amplifying samples using PCR")
-    lab.amplify(['a', 'c', 't', 'g'], rounds=5)
+    lab.amplify(['a', 'c', 't', 'g'], rounds=6)
     lab.split()
 
     # Merge all our dna samples
     print("[*] Merging our samples")
-    lab.merge()
+    lab.merge(log=True)
 
     # Search the sample for satisfying dna to the formula. Correct size and satisfiable
     print("[*] Filtering the unwanted data")
     lab.length_sort()
     expected_size = (SIZE*len(variables), SIZE*len(variables))
     lab.filter_by_length(expected_size)
+    tmp = copy.deepcopy(lab)
     satisfiable = filter_by_sat(lab, sat).samples != []
 
+
+    # Log result and return its value
+    print("[RESULT] {} is {}satisfiable".format(formula, "" if satisfiable else "not "))
     return satisfiable
 
-formula = "(avbvc)^(~av~bvc)"
-satisfiable = three_sat(formula)
-print("{} is {} satisfiable".format(formula, "" if satisfiable else "not"))
+if __name__ == "__main__":
+    # formula = "(avbvc)^(~av~bvc)"
+    # satisfiable = three_sat(formula)
+
+    formula = "(avava)^(~av~av~b)"
+    satisfiable = three_sat(formula)
